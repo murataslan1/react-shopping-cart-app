2ee817f2a784fd33b3a0e0028a83f8366fcce231
Murat Aslan <78961478+murataslan1@users.noreply.github.com>
2022-07-03 20:27:08 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# react-shopping-cart-app
-Created with CodeSandbox
+Youtube  : https://www.youtube.com/watch?v=eSmRCGcMzC0&t=610s&ab_channel=BurakK%C3%B6m%C3%BCrc%C3%BC
---
+++ src/App.js
@@ -0,0 +1,40 @@
+import Basket from "./components/Basket";
+import Header from "./components/Header";
+import Product from "./components/Product";
+import "./index.css";
+import products from "./products.json";
+import { useState, useEffect } from "react";
+function App() {
+  const [basket, setBasket] = useState([]);
+  const [cost, setCost] = useState("");
+
+  useEffect(() => {
+    const totalPrice = basket.reduce(
+      (pre, basket) => pre + basket.amount * basket.price,
+      0
+    );
+    setCost(totalPrice);
+  }, [basket]);
+
+  return (
+    <>
+      <Header />
+
+      <div className="productContainer">
+        <div className="productBoxs">
+          {products.map((product) => (
+            <Product
+              key={product.id}
+              basket={basket}
+              setBasket={setBasket}
+              product={product}
+            />
+          ))}
+        </div>
+        <Basket cost={cost} basket={basket} />
+      </div>
+    </>
+  );
+}
+
+export default App;
